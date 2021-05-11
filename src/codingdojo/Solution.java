411a99b16e2276db2e73cc6740dd8ebd0c4b1482
Silvana Pedraza <silvana.pedraza@diconium.com>
2021-05-12 01:22:19 +0200

    Initial commit

---
+++ src/codingdojo/Solution.java
@@ -0,0 +1,10 @@
+package codingdojo;
+/*
+Challenge: https://leetcode.com/problems/unique-morse-code-words/
+For playing: https://morsecode.world/international/translator.html
+ */
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        return 0;
+    }
+}
---
+++ src/codingdojo/SolutionTest.java
@@ -0,0 +1,35 @@
+package codingdojo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    Solution toTest = new Solution();
+
+    @Test
+    void uniqueMorseRepresentations1() {
+        assertEquals(2,toTest.uniqueMorseRepresentations(new String[]{"gin", "zen", "gig", "msg"}));
+    }
+
+    @Test
+    void uniqueMorseRepresentations2() {
+        assertEquals(1,toTest.uniqueMorseRepresentations(new String[]{"hello", "ssdrj", "isedrj", "svirwt", "eiilram"}));
+    }
+
+    @Test
+    void uniqueMorseRepresentations3() {
+        assertEquals(7,toTest.uniqueMorseRepresentations(new String[]{"one", "two", "three", "four", "five", "six", "seven"}));
+    }
+
+    @Test
+    void uniqueMorseRepresentations4() {
+        assertEquals(4,toTest.uniqueMorseRepresentations(new String[]{"hello", "ssdrj", "silvana", "sirsrga", "alongerwordd", ""}));
+    }
+
+    @Test
+    void uniqueMorseRepresentations5() {
+        assertEquals(0,toTest.uniqueMorseRepresentations(new String[]{}));
+    }
+}
